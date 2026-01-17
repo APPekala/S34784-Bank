29fba323c1d435f9bdedb2adde1e06a81854d0eb
APPekala <s34784@pjwstk.edu.pl>
2026-01-17 14:44:02 +0100

    Update BankService.java

--- main/java/pl/edu/s34784/s34784_bank/service/BankService.java
+++ main/java/pl/edu/s34784/s34784_bank/service/BankService.java
@@ -1,7 +1,9 @@
 package pl.edu.s34784.s34784bank.service;
 
 import org.springframework.stereotype.Service;
-import pl.edu.s34784.s34784bank.model.*;
+import pl.edu.s34784.s34784bank.model.Client;
+import pl.edu.s34784.s34784bank.model.TransactionResponse;
+import pl.edu.s34784.s34784bank.model.TransactionStatus;
 
 import java.math.BigDecimal;
 import java.util.HashMap;
@@ -10,7 +12,6 @@ import java.util.concurrent.atomic.AtomicLong;
 
 @Service
 public class BankService {
-
     private final Map<Long, Client> clients = new HashMap<>();
     private final AtomicLong idGenerator = new AtomicLong(1);
 
@@ -23,26 +24,21 @@ public class BankService {
 
     public TransactionResponse deposit(Long clientId, BigDecimal amount) {
         Client client = clients.get(clientId);
-
         if (client == null || amount.compareTo(BigDecimal.ZERO) <= 0) {
             return new TransactionResponse(TransactionStatus.DECLINED, null);
         }
-
         client.setBalance(client.getBalance().add(amount));
         return new TransactionResponse(TransactionStatus.ACCEPTED, client.getBalance());
     }
 
     public TransactionResponse transfer(Long clientId, BigDecimal amount) {
         Client client = clients.get(clientId);
-
         if (client == null || amount.compareTo(BigDecimal.ZERO) <= 0) {
             return new TransactionResponse(TransactionStatus.DECLINED, null);
         }
-
         if (client.getBalance().compareTo(amount) < 0) {
             return new TransactionResponse(TransactionStatus.DECLINED, client.getBalance());
         }
-
         client.setBalance(client.getBalance().subtract(amount));
         return new TransactionResponse(TransactionStatus.ACCEPTED, client.getBalance());
     }
